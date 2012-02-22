6b955cc7ef61b3a45a3daf5b7c83005830ec2fa8
Jeremy Rottman <rottmanj@elguapodev.com>
2012-02-22 00:26:28 -0800

    Converted building stats from integers to decimals and changed the damage_type and armor_type to use modifiers

--- db/migrate/20120218074423_create_buildings_level_defensive_stats.rb
+++ db/migrate/20120218074423_create_buildings_level_defensive_stats.rb
@@ -1,9 +1,9 @@
 class CreateBuildingsLevelDefensiveStats < ActiveRecord::Migration
   def change
     create_table :buildings_level_defensive_stats do |t|
-      t.integer :armor_points, :null=>false
-      t.integer :health_points, :null=>false
-      t.integer :armor_type, :null=>false
+      t.decimal :armor_points, :precision => 15, :scale => 10, :default=>'0.0'
+      t.decimal :health_points, :precision => 15, :scale => 10, :default=>'0.0'
+      t.integer :armor_type_id, :null=>false
       t.boolean :is_reinforced, :default=>false
       t.integer :level_id
       t.timestamps
